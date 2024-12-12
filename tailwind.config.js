eb545c1b79f6057132f782f3a18182c5cf2fad73
Dhanu <avuladharaneeswarreddy123@gmail.com>
2024-12-12 17:20:06 +0530

    newchanges

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,25 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+// module.exports = {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}", // Add all files with JS, JSX, TS, TSX extensions
+//   ],
+//   theme: {
+//     extend: {
+//       backgroundImage: {
+//         'steel-gradient': 'linear-gradient(135deg, #434343, #3a3a3a, #888888)',
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
+export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // Add all files with JS, JSX, TS, TSX extensions
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'steel-gradient': 'linear-gradient(135deg, #434343, #3a3a3a, #888888)',
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
